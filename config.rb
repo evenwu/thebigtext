b4aa1c223458b5318eb736452ac864776d31f4ba
evenwu <evendesign@gmail.com>
2014-04-08 20:03:18 +0800

    init

---
+++ config.rb
@@ -0,0 +1,25 @@
+require 'slim'
+# Require any additional compass plugins here.
+
+
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
+the_hold_options = {:login=>"evenwu", :token=>"229f2b40d4f17f98f142318250a75f20", :project=>"devcore", :project_site_password=>"123456", :host=>"http://the-hold.handlino.com/", :cname=>"devcore.evendesign.tw"}
